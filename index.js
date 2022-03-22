cac60e6ca0409ff828561d826870b8ecc59775a6
Андрей Усанов <andrei0usanov@gmail.com>
2022-03-22 09:46:41 +0300

    fix cars

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post('/v1/authorization', (req, res) => {
 })
 
 app.get('/v1/cars', (req, res) => {
-    const cars = [{
+    const vehicles = [{
         id: 1,
         model: 'm3',
         price: 10000000,
@@ -51,7 +51,7 @@ app.get('/v1/cars', (req, res) => {
         description: null,
         brandName: 'BMW'
     }]
-    res.render('table', { taskList: cars });
+    res.render('table', { taskList: vehicles });
 })
 
 app.use((req, res, next) => {
